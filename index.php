b65f818f9b6aa97eb3ca9017f0fbc5fc0c97432e
codepipe <47555726+codepipe@users.noreply.github.com>
2021-08-07 14:44:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 $t = date("H");
 
 if ($t < "20") {
-  echo "Have a good day with coding skills!";
+  echo "Have a good day with coding skills!, this is new code adding in nthe index file just for cross check wheter the sns topic is notifing or not";
 }
 ?>
  
